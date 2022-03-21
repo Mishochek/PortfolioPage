0ee82194743556a09736d11438c1fa1eeb8743f0
Mishcok <63130553+Mishochek@users.noreply.github.com>
2022-03-21 19:30:01 +0300

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,11 @@ let random = function (event) {
 };
 
 let changeBody = function(event) {
-    let randomColor = 'rgb(' + random() + ',' + random() + ',' + random() + ')';
+    let first = random();
+    let second = random();
+    let third = random();
+    let randomColor = 'rgb(' + first + ',' + second + ',' + third + ')';
     document.body.style.backgroundColor = randomColor;
+    document.getElementById('logo').style.color = 'rgb(' + Math.abs(255 - first) + ',' + Math.abs(255 - second) + ',' + Math.abs(255 - third) + ')';
 }
 dido.addEventListener('click', changeBody)
